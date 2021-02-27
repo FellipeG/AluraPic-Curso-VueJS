ff90ca8c3c737724a3ba5964731803bf15011f9b
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2021-02-26 23:41:57 -0300

    utilizando v-for para iterar o array

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt="foto.titulo" />
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+    
   </div>
 </template>
 
@@ -12,10 +17,16 @@ export default {
 
     return {
       titulo: "Alurapic",
-      foto: {
-        url: "https://static1.patasdacasa.com.br/articles/8/10/38/@/4864-o-cachorro-inteligente-mostra-essa-carac-opengraph_1200-1.jpg",
-        titulo: "cachorro"
-      }
+      fotos: [
+        {
+          url: "https://static1.patasdacasa.com.br/articles/8/10/38/@/4864-o-cachorro-inteligente-mostra-essa-carac-opengraph_1200-1.jpg",
+          titulo: "cachorro"
+        },
+        {
+          url: "https://static1.patasdacasa.com.br/articles/8/10/38/@/4864-o-cachorro-inteligente-mostra-essa-carac-opengraph_1200-1.jpg",
+          titulo: "cachorro2"
+        }
+      ]
     }
 
   }
