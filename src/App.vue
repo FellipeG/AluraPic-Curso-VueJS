cdf1d559991ee7d015e8b26d574d81d6af8254fe
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2021-03-02 23:44:48 -0300

    Criando o componente Painel para ser reutilizado na aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,69 @@
 <template>
-  <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-          <div class="painel-conteudo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
-          </div>
-        </div>
-      </li>
-    </ul>
-    
-  </div>
+	<div class="corpo">
+		<h1 class="centralizado">{{ titulo }}</h1>
+		<ul class="lista-fotos">
+			<li class="lista-fotos-item" v-for="foto of fotos">
+				<meu-painel :titulo="foto.titulo">
+					<img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+				</meu-painel>
+			</li>
+		</ul>
+		
+	</div>
 </template>
 
 <script>
+
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
 
-  data() {
+	components: {
+		'meu-painel': Painel
+	},
 
-    return {
-      titulo: "Alurapic",
-      fotos: []
-    }
+	data() {
 
-  },
-  
-  created() {
+		return {
+			titulo: "Alurapic",
+			fotos: []
+		}
+
+	},
+	
+	created() {
 
 
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
-  }
-  
+		this.$http.get('http://localhost:3000/v1/fotos')
+			.then(res => res.json())
+			.then(fotos => this.fotos = fotos, err => console.log(err));
+	}
+	
 }
 </script>
 
 <style>
 
-  .corpo {
-    font-family: Helvetica, sans-serif;
-    width: 96%;
-    margin: 0 auto;
-  }
+	.corpo {
+		font-family: Helvetica, sans-serif;
+		width: 96%;
+		margin: 0 auto;
+	}
 
-  .centralizado {
-    text-align: center;
-  }
+	.centralizado {
+		text-align: center;
+	}
 
-  .lista-fotos {
-    list-style: none;
-  }
+	.lista-fotos {
+		list-style: none;
+	}
 
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
+	.lista-fotos .lista-fotos-item {
+		display: inline-block;
+	}
 
-  .imagem-responsiva {
-    width: 100%;
-  }
-
-   /* estilo do painel */ 
-
-  .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
+	.imagem-responsiva {
+		width: 100%;
+	}
 
 </style>
