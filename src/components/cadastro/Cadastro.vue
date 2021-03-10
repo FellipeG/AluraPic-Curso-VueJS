79060e61421653e24ec031b5c378c2b2061b817e
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2021-03-09 22:15:06 -0300

    Criando componente de cadastro + criando componente Home + instalando VueRouter

---
+++ src/components/cadastro/Cadastro.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2 class="centralizado">Cadastro</h2>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+    .centralizado {
+        text-align: center;
+    }
+
+</style>
